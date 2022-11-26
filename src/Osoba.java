699b9da6865d926c0b9f1fe73e83e6abc9e6d087
johnszyna <jan.szyndlarewicz@gmail.com>
2022-11-27 00:40:02 +0100

    Abstract osoba set/get

---
+++ src/Osoba.java
@@ -0,0 +1,43 @@
+public abstract class Osoba {
+    private String name, second_name, bitrhdate;
+
+    public Osoba(){
+        name = "";
+        second_name = "";
+        bitrhdate = "01.01.1900";
+    }
+
+    public Osoba(String name, String second_name, String bitrhdate) {
+        this.name = name;
+        this.second_name = second_name;
+        this.bitrhdate = bitrhdate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSecond_name() {
+        return second_name;
+    }
+
+    public void setSecond_name(String second_name) {
+        this.second_name = second_name;
+    }
+
+    public String getBitrhdate() {
+        return bitrhdate;
+    }
+
+    public void setBitrhdate(String bitrhdate) {
+        this.bitrhdate = bitrhdate;
+    }
+
+    public String toString(){
+        return "Imię: " + getName() + ", nazwisko: " + getSecond_name() + ", urodziny: " + getBitrhdate();
+    }
+}
